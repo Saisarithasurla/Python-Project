3796b0e6ec9895c60873aace5e4b15ea4368f28f
Saisarithasurla <139557871+Saisarithasurla@users.noreply.github.com>
2025-10-16 19:39:55 +0530

    Update my_config.py

--- Python Assistant Project/my_config.py
+++ Python Assistant Project/my_config.py
@@ -1,5 +1,6 @@
 # my_config.py
 
-OPENWEATHER_API_KEY = "d1089024e29a266e05264b9715673a2e"
+OPENWEATHER_API_KEY = "d1089024e29a266e05654esw9715673a2e"
 QUOTES_API_URL = "https://zenquotes.io/api/random"
+
 
